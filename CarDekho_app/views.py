8915a4e7bf3f739dfe780961386e9ad15ce8daec
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-16 18:16:21 +0530

    Serialization & Function Based View

--- CarDekho_app/views.py
+++ CarDekho_app/views.py
@@ -1,25 +1,42 @@
 from django.shortcuts import render
 from .models import Carlist
 from django.http import JsonResponse
-from django.http import HttpResponse
-import json
+# from django.http import HttpResponse
+# import json
+
+from .api_file.serializers import CarSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+# def car_list_view(request):
+#     cars = Carlist.objects.all()
+#     data = {
+#         'cars' : list(cars.values()),
+#     }
+#     data_json = json.dumps(data)
+#     return HttpResponse(data_json, content_type='application/json')
+#     # return JsonResponse(data)
+
+
+# def car_detail_view(request, pk):
+#     car = Carlist.objects.get(pk=pk)
+#     data = {
+#         'name' : car.name,
+#         'description' : car.description,
+#         'active' : car.active
+#     }
+#     return JsonResponse(data)
+
+@api_view()
 def car_list_view(request):
-    cars = Carlist.objects.all()
-    data = {
-        'cars' : list(cars.values()),
-    }
-    data_json = json.dumps(data)
-    return HttpResponse(data_json, content_type='application/json')
-    # return JsonResponse(data)
+    car = Carlist.objects.all()
+    serializer = CarSerializer(car, many=True)
+    return Response(serializer.data)
 
 
+@api_view()
 def car_detail_view(request, pk):
     car = Carlist.objects.get(pk=pk)
-    data = {
-        'name' : car.name,
-        'description' : car.description,
-        'active' : car.active
-    }
-    return JsonResponse(data)
+    serializer = CarSerializer(car)
+    return Response(serializer.data)
