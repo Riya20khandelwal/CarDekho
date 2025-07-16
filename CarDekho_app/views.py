1c64c2314763eed9aa24a519d3a3f481848c7d5e
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-16 17:41:26 +0530

    Creating Client in Django

--- CarDekho_app/views.py
+++ CarDekho_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Carlist
 from django.http import JsonResponse
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 def car_list_view(request):
@@ -8,7 +10,9 @@ def car_list_view(request):
     data = {
         'cars' : list(cars.values()),
     }
-    return JsonResponse(data)
+    data_json = json.dumps(data)
+    return HttpResponse(data_json, content_type='application/json')
+    # return JsonResponse(data)
 
 
 def car_detail_view(request, pk):
