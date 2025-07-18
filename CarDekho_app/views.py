e7e1debb24425ea4d5abec819adf756d536c5b53
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-18 16:52:06 +0530

    Concrete View Classes || CreateAPIView, ListAPIView & DestroyAPIView

--- CarDekho_app/views.py
+++ CarDekho_app/views.py
@@ -133,24 +133,35 @@ class Showroom_Details(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+# class ReviewList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     # Model Authentication
+#     authentication_classes = [SessionAuthentication]
+#     permission_classes = [DjangoModelPermissions]
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+    
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+    
+# class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+#     queryset = Review.objects.all()
+#     serializer_class = ReviewSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+
+# Concrete View Class
+class ReviewList(generics.ListCreateAPIView):
 
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
-    # Model Authentication
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [DjangoModelPermissions]
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class ReviewDetail(mixins.RetrieveModelMixin, generics.GenericAPIView):
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
