43282900c95e7a81c7b93a17940a04afe2f61c73
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-22 16:07:56 +0530

    Generating TOken by sending User Data in URL

--- CarDekho/urls.py
+++ CarDekho/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('car/', include('CarDekho_app.urls')),
-    path('auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('account/', include('user_app.api.urls')),
+    # path('auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
