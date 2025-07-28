701f711e588d9b168d4217403f4f827fff1d6ef8
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-28 14:33:54 +0530

    Deleting TOKEN when USER get Logout

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,7 +1,15 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 from user_app.api.serializers import RegisterSerializer
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+
+@api_view(['POST',])
+@authentication_classes([TokenAuthentication])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST',])
 def registration_view(request):
